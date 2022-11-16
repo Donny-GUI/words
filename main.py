48ad37b8798525f579648023b6f625c95a824704
shipit <108424001+Donny-GUI@users.noreply.github.com>
2022-11-15 20:33:36 -0800

    Update main.py

--- main.py
+++ main.py
@@ -10,7 +10,6 @@ punctuationx = '!?.,:;"[]}{()*&^%$#~\n\t'
 punctuation = [x for x in punctuationx]
 
 def get_page_about(topic):
-
     wikipedia_helper = wikipediaapi.Wikipedia(
         language='en',extract_format=wikipediaapi.ExtractFormat.WIKI)
     wikipedia_page = wikipedia_helper.page(topic)
@@ -18,23 +17,16 @@ def get_page_about(topic):
 
 def wikipedia_page_to_word_list(page):
     mypage = str(page)
-
     current_word = []
-
     words = []
-
     for character in mypage:
-
         current_char = character
-
         if current_char in ALPHA:
             current_word.append(current_char)
             continue
-
         if current_char in alpha:
             current_word.append(current_char)
-            continue
-        
+            continue   
         if current_char == " ":
             if current_word != "":
                 words.append("".join(current_word).strip())
@@ -42,7 +34,6 @@ def wikipedia_page_to_word_list(page):
                 continue
             else:
                 continue
-
         if current_char in punctuation:
             if current_word != []:
                 words.append("".join(current_word).strip())
@@ -50,13 +41,9 @@ def wikipedia_page_to_word_list(page):
                 continue  
             else:
                 continue
-
     non_repeating_words = set(words)
-
     plist = list(non_repeating_words)
-
     mywords2have = []
-
     for word in plist:
         length = len(word)
         if length < 2:
@@ -64,44 +51,22 @@ def wikipedia_page_to_word_list(page):
         else:
             xword = str(word).lower()
             mywords2have.append(xword)
-
     return mywords2have
 
-def show_word_collection(_words):
-    for word in _words:
-        print(word)
-
 def parse_words_to_file(words):
-
     curdir = os.getcwd()
-
     for word in words:
-
         first = word[0]
         second = word[1]
-
         super_path = f"{curdir}\\words\\{first}\\{first}{second}" 
-
         with open(super_path, "a") as afile:
             xword = str(word).strip()
             afile.write(f"{xword}\n")
-        
         print(f"[ {xword} ] added to file: {super_path}")
 
-
-dolphs = get_page_about("Dolphins")
-
-mywords = wikipedia_page_to_word_list(dolphs)
-
-parse_words_to_file(mywords)
-
-
 def get_words_about(this_topic):
-
     mytopic = get_page_about(this_topic)
-
     words_on_topic = wikipedia_page_to_word_list(mytopic)
-
     parse_words_to_file(words_on_topic)
     
 
@@ -113,5 +78,5 @@ if __name__ == '__main__':
         get_words_about(xtopic)
     except:
         print("please specify the topic as an argument")
-        print("Ex: python3 main.py 'Max Keeble's big move' ")
+        print("""Ex: python3 main.py "Max Keeble's big move" """)
 
