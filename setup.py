4e13bcc7e78b1abc215a683db0be5ce5e26596c9
shipit <108424001+Donny-GUI@users.noreply.github.com>
2022-11-21 09:26:48 -0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,24 +5,73 @@
 #
 
 import os
+import sys
+import platform
 
-alpha = "abcdefghijklmnopqrstuvwxyz"
+print(sys.platform, "Detected")
 
-current_dir = os.getcwd()
 
-first_path = f"{current_dir}\\words"
 
-os.mkdir(f"{current_dir}\\words")
+def windows_install():
+    alpha = "abcdefghijklmnopqrstuvwxyz"
 
-for char in alpha:
+    current_dir = os.getcwd()
 
-    subdir = f"{first_path}\\{char}"
+    first_path = f"{current_dir}\\words"
 
-    os.mkdir(subdir)
+    os.mkdir(f"{current_dir}\\words")
 
-    for second in alpha:
+    print("[NEW DIR]", f"{current_dir}\\words")
 
-        filename = f"{subdir}\\{char}{second}"
+    for char in alpha:
 
-        with open(filename, "w") as wfile:
-            wfile.close()
+        subdir = f"{first_path}\\{char}"
+
+        os.mkdir(subdir)
+
+        for second in alpha:
+
+            filename = f"{subdir}\\{char}{second}"
+
+            with open(filename, "w") as wfile:
+                wfile.close()
+            print("[NEW FILE]", filename)
+
+
+def linux_install():
+    alpha = "abcdefghijklmnopqrstuvwxyz"
+
+    current_dir = os.getcwd()
+
+    first_path = f"{current_dir}/words"
+
+    os.mkdir(f"{current_dir}/words")
+
+    print("[NEW DIR]", f"{current_dir}/words")
+
+    for char in alpha:
+
+        subdir = f"{first_path}/{char}"
+
+        os.mkdir(subdir)
+
+        for second in alpha:
+
+            filename = f"{subdir}/{char}{second}"
+
+            with open(filename, "w") as wfile:
+                wfile.close()
+            print("[NEW FILE]", filename)
+
+
+
+
+
+match sys.platform:
+    case 'linux':
+        linux_install()
+    case 'windows':
+        windows_install()
+    case 'darwin':
+        print("MacOS not currently supported")
+
